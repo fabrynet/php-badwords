5c37aa74cbe34d56d20d3b2ea199e6ed204de7ae
Fabrizio <fabrynet.bianco@gmail.com>
2020-09-02 09:40:09 +0200

    Sistemazioni al codice.

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
-<?php 
+<?php
+
+// Creare una variabile con un paragrafo di testo.
+// Visualizzare a schermo il paragrafo con la relative lunghezza e sostituire la badword passata in GET con tre *.
+
 $parag = "Ciao Fabrizio, come stai? Il tuo nome è Fabrizio";
+
 $badword = $_GET["badword"];
-$parag_bad = str_replace($badword, "***", $parag);
+$goodword = "***";
+
+$parag_good = str_replace($badword, $goodword, $parag);
 $parag_len = strlen($parag_bad);
-echo $parag_bad;
+
+echo $parag_good;
 echo "<br>";
 echo "Lunghezza: ". $parag_len. " caratteri.";
+
 ?>
